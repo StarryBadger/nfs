aa85016fc2dc644f0afc5a34e723770ee063fedc
StarryBadger <shaunak.biswas@research.iiit.ac.in>
2023-11-12 12:19:50 +0530

    searched for all files in the given directory

--- headers.h
+++ headers.h
@@ -1,14 +1,27 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <limits.h>
+#include <linux/limits.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <pwd.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <semaphore.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "trie.h"
 
@@ -22,4 +35,4 @@ typedef struct MessageSS2NM
     char buffer[MAX_PATH_LENGTH];
     int port_for_clients;
     int port_for_naming_server;
-}MessageSS2NM;
+} MessageSS2NM;
