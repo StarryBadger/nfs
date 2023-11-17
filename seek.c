01021637b624d632057c0ffbb9d6565fee29e274
StarryBadger <shaunak.biswas@research.iiit.ac.in>
2023-11-17 23:16:22 +0530

    added file and directory identifiers in trie to string encoding

--- seek.c
+++ seek.c
@@ -1,8 +1,17 @@
 #include "headers.h"
-
-void lookFor(char *path, int pathlength, TrieNode* root)
+void appendCharToFront(char *str, char character)
+{
+    int length = strlen(str);
+    for (int i = length-1; i >= 0; i--)
+    {
+        str[i + 1] = str[i];
+    }
+    str[0] = character;
+}
+void lookFor(char *path, int pathlength, TrieNode *root)
 {
     DIR *directory = opendir(path);
+    char TEMP_BUFF[PATH_MAX];
     if (directory == NULL)
     {
         fprintf(stderr, "\x1b[31mCould not open %s. Permission denied\n\n\x1b[0m", path);
@@ -11,12 +20,12 @@ void lookFor(char *path, int pathlength, TrieNode* root)
     struct dirent *entry;
     while ((entry = readdir(directory)) != NULL)
     {
-        if (strcmp(entry->d_name, ".")==0 || strcmp(entry->d_name, "..")==0)
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
         {
             continue;
         }
         // hidden files should not be consdiered
-        if (entry->d_name[0] == '.' || (strlen(entry->d_name) > 1 && entry->d_name[1] == '.' ))
+        if (entry->d_name[0] == '.' || (strlen(entry->d_name) > 1 && entry->d_name[1] == '.'))
         {
             continue;
         }
@@ -31,14 +40,17 @@ void lookFor(char *path, int pathlength, TrieNode* root)
         if (S_ISDIR(st.st_mode))
         {
             printf("%s\n", pathBranch + pathlength);
-            InsertTrie(pathBranch + pathlength ,root);
-            lookFor(pathBranch, pathlength, root);       
+            strcpy(TEMP_BUFF,pathBranch + pathlength);
+            appendCharToFront(TEMP_BUFF,'D');
+            InsertTrie(TEMP_BUFF, root);
+            lookFor(pathBranch, pathlength, root);
         }
         else if (S_ISREG(st.st_mode))
         {
-            printf("%s\n", pathBranch + pathlength );
-            InsertTrie(pathBranch + pathlength ,root);
-
+            printf("%s\n", pathBranch + pathlength);
+            strcpy(TEMP_BUFF,pathBranch + pathlength);
+            appendCharToFront(TEMP_BUFF,'F');
+            InsertTrie(TEMP_BUFF, root);
         }
     }
     closedir(directory);
