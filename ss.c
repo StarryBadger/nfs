bae64430a39880b1f732f87976a0b4cd5e55356d
Shreyansh Bhanu <saurabhshreyansh868@gmail.com>
2023-11-21 15:03:07 +0530

    added redundancy part in ss and now port is being sent

--- ss.c
+++ ss.c
@@ -3,9 +3,11 @@
 int port_for_clients;
 int port_for_naming_server;
 int port_for_naming_server_np;
+int port_for_naming_server_Red;
 sem_t portc_lock;
 sem_t portnms_lock;
 sem_t portnmsNp_lock;
+sem_t portnmRed_lock;
 int close_flag = 0;
 TrieNode *ssTrie = NULL;
 pthread_t nm_ss_connection;
@@ -97,6 +99,7 @@ void *naming_server_informer_worker(void *arg)
     sem_wait(&portc_lock);
     sem_wait(&portnms_lock);
     sem_wait(&portnmsNp_lock);
+    sem_wait(&portnmRed_lock);
     int ss_sock;
     struct sockaddr_in addr;
     socklen_t addr_size;
@@ -140,7 +143,7 @@ void *naming_server_informer_worker(void *arg)
     message.port_for_clients = port_for_clients;
     message.port_for_naming_server = port_for_naming_server;
     message.port_for_nm_np = port_for_naming_server_np;
-
+    message.port_for_nm_red=port_for_naming_server_Red;
     printf("Sending message to server: %d %d %d\n", message.port_for_clients, message.port_for_naming_server, message.port_for_nm_np);
 
     if (send(ss_sock, &message, sizeof(message), 0) < 0)
@@ -713,16 +716,223 @@ void GetAccessiblePaths()
         // GetAccessiblePaths();
     }
 }
+
+void *NMServerREDConnection(void *arg)
+{
+    int server_sock, nms_sock;
+    struct sockaddr_in server_addr, nms_addr;
+    socklen_t addr_size;
+    // char buffer[PATH_MAX];
+    int n;
+
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0)
+    {
+        fprintf(stderr, "[-]Socket creation error: %s\n", strerror(errno));
+        // exit(1);
+    }
+    printf("[+]TCP server socket created.\n");
+
+    memset(&server_addr, '\0', sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = 0;
+    server_addr.sin_addr.s_addr = inet_addr(ip_address);
+
+    n = bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (n < 0)
+    {
+        fprintf(stderr, "[-]Bind error: %s\n", strerror(errno));
+        close(server_sock);
+        // exit(1);
+    }
+
+    struct sockaddr_in sin;
+    socklen_t len = sizeof(sin);
+    while (1)
+    {
+        if (getsockname(server_sock, (struct sockaddr *)&sin, &len) == -1)
+        {
+            fprintf(stderr, "couldn't extract port of socket error\n");
+            continue;
+        }
+        else
+            break;
+    }
+    port_for_naming_server_Red = sin.sin_port;
+    printf("port extracted is %d\n", port_for_clients);
+    sem_post(&portnmRed_lock);
+    if (listen(server_sock, 5) < 0)
+    {
+        fprintf(stderr, "[-]Storage server got disconnected from Naming Server %s\n", strerror(errno));
+        close(server_sock);
+        exit(1);
+    }
+    printf("listening to respond to naming server\n");
+
+    // addr_size = sizeof(nms_addr);
+
+    // nms_sock = accept(server_sock, (struct sockaddr *)&nms_addr, &addr_size);
+    // if (nms_sock < 0)
+    // {
+    //     fprintf(stderr, "[-]Accept error: %s\n", strerror(errno));
+    //     if (close(server_sock) < 0)
+    //         fprintf(stderr, "[-]Error closing socket: %s\n", strerror(errno));
+    //     // exit(1);
+    // }
+    while (1)
+    {
+        addr_size = sizeof(nms_addr);
+
+        nms_sock = accept(server_sock, (struct sockaddr *)&nms_addr, &addr_size);
+        if (nms_sock < 0)
+        {
+            fprintf(stderr, "[-]Accept error: %s\n", strerror(errno));
+            if (close(server_sock) < 0)
+                fprintf(stderr, "[-]Error closing socket: %s\n", strerror(errno));
+            // exit(1);
+        }
+        printf("Accepted\n");
+        MessageClient2SS message;
+        message.operation = 0;
+        bzero(message.buffer, PATH_MAX);
+        bzero(message.msg, PATH_MAX);
+        // FILE *this = fopen("this.txt", "w");
+        int bytes_read;
+        if ((bytes_read = recv(nms_sock, &message, sizeof(message), 0)) < 0)
+        {
+            fprintf(stderr, "[-]Receive error: %s\n", strerror(errno)); // ERROR HANDLING
+            // if (close(nms_sock) < 0)
+            //     fprintf(stderr, "[-]Error closing socket: %s\n", strerror(errno)); // ERROR HANDLING
+            // exit(1);
+            continue;
+        }
+        printf("doen recv00;\n");
+        // fwrite(message.msg, 1, strlen(message.msg), this);
+        // fclose(this);
+        if (message.operation == 0 || bytes_read == 0)
+            continue;
+        // printf("the message : %d\n", strlen(message.buffer));
+
+        if (message.operation == CREATE && !message.isADirectory)
+        {
+            printf("one\n");
+            int err_code;
+            int fd = open(message.buffer, O_CREAT | O_WRONLY, 0644);
+            if (fd == -1)
+            {
+                fprintf(stderr, "\x1b[31mCould not open %s. Permission denied\n\n\x1b[0m", message.buffer); // ERROR HANDLING
+                err_code = FILE_UNABLE_TO_CREATE;
+                // return NULL;
+            }
+            // InsertTrie(message.buffer, ssTrie); //SHREYANSH
+            err_code = NO_ERROR;
+
+            if (send(nms_sock, &err_code, sizeof(err_code), 0) < 0)
+            {
+                fprintf(stderr, "[-]Send time error: %s\n", strerror(errno)); // ERROR HANDLING
+                if (close(nms_sock) < 0)
+                    fprintf(stderr, "[-]Error closing socket: %s\n", strerror(errno)); // ERROR HANDLING
+                // exit(1);
+                continue;
+            }
+
+            if (err_code == FILE_UNABLE_TO_CREATE)
+                return NULL;
+
+            close(fd);
+        }
+
+        if (message.operation == CREATE && message.isADirectory)
+        {
+            printf("two\n");
+            int err_code;
+            if (mkdir(message.buffer, 0777) == -1)
+            {
+                fprintf(stderr, "\x1b[31mCould not create %s. Permission denied\n\n\x1b[0m", message.buffer); // ERROR HANDLING
+                err_code = DIRECTORY_UNABLE_TO_CREATE;
+                // return NULL;
+            }
+            // InsertTrie(message.buffer, ssTrie); // SHREYANSH
+            err_code = NO_ERROR;
+
+            if (send(nms_sock, &err_code, sizeof(err_code), 0) < 0)
+            {
+                fprintf(stderr, "[-]Send time error: %s\n", strerror(errno)); // ERROR HANDLING
+                if (close(nms_sock) < 0)
+                    fprintf(stderr, "[-]Error closing socket: %s\n", strerror(errno)); // ERROR HANDLING
+                exit(1);
+            }
+
+            if (err_code == DIRECTORY_UNABLE_TO_CREATE)
+                return NULL;
+        }
+
+        if (message.operation == DELETE)
+        {
+            // int fd = open(message.buffer, O_RDONLY);
+            // if (fd == -1)
+            // {
+            //     fprintf(stderr, "\x1b[31mCould not open %s. Permission denied\n\n\x1b[0m", message.buffer); // ERROR HANDLING
+            //     return NULL;
+            // }
+            // close(fd);
+            int err_code = NO_ERROR;
+            printf("to be deleted: %s\n", message.buffer);
+            if (remove(message.buffer) == 0)
+            {
+                printf("\x1b[32mDeleted %s successfully\n\n\x1b[0m", message.buffer);
+                // DeleteTrie(message.buffer, ssTrie);
+            }
+            else
+            {
+                printf("\x1b[31mCould not delete %s\n\n\x1b[0m", message.buffer);
+                err_code = UNABLE_TO_DELETE;
+            }
+
+            if (send(nms_sock, &err_code, sizeof(err_code), 0) < 0)
+            {
+                fprintf(stderr, "[-]Send time error: %s\n", strerror(errno)); // ERROR HANDLING
+                if (close(nms_sock) < 0)
+                    fprintf(stderr, "[-]Error closing socket: %s\n", strerror(errno)); // ERROR HANDLING
+                exit(1);
+            }
+        }
+
+        int err_code = NO_ERROR;
+
+        if (message.operation == READ)
+        {
+            // int fd = open(message.buffer, O_RDONLY);
+            FILE *file = fopen(message.buffer, "r");
+            Read_ss(&err_code, nms_sock, message, file, 0);
+            fclose(file);
+        }
+
+        if (message.operation == WRITE)
+        {
+            // int fd = open(message.buffer, O_WRONLY | O_TRUNC);
+            FILE *fd = fopen(message.buffer, "a");
+            Write_ss(&err_code, nms_sock, message, fd, 0);
+            fclose(fd);
+        }
+        close(nms_sock);
+    }
+    
+    if (close_flag == 1)
+        return NULL;
+}
 int main()
 {
     GetAccessiblePaths();
-    pthread_t clients_handler, naming_server_informer, naming_server_responder;
+    pthread_t clients_handler, naming_server_informer, naming_server_responder,naming_server_red;
     sem_init(&portc_lock, 0, 0);
     sem_init(&portnms_lock, 0, 0);
     sem_init(&portnmsNp_lock, 0, 0);
+    sem_init(&portnmRed_lock,0,0);
     pthread_create(&clients_handler, NULL, clients_handler_worker, NULL);
     pthread_create(&naming_server_responder, NULL, naming_server_responder_worker, NULL);
     pthread_create(&naming_server_informer, NULL, naming_server_informer_worker, NULL);
+    pthread_create(&naming_server_red,NULL,NMServerREDConnection,NULL);
     while (1)
     {
     }
